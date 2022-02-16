b8898c38dd54a4ec8c6715cc1d4bcc1d604cc77a
mizuno <31834@toyota.kosen-ac.jp>
2022-02-17 00:26:50 +0900

    Add: edit function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,4 +12,13 @@ class PostsController < ApplicationController
     @post.save
     redirect_to("/posts")
   end
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  def update 
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts")
+  end
 end
