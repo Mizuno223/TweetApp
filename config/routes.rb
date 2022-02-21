6b9a836caef969156a9b5556497a1a5d04e2a49d
mizuno <31834@toyota.kosen-ac.jp>
2022-02-21 23:39:45 +0900

    Add: user detail page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'user/index'
   get "/" => "home#top"
   get "about" => "home#about"
+
   get "posts" => "posts#index"
   get "posts/new" => "posts#new"
   post "posts/create" => "posts#create"
@@ -10,6 +11,8 @@ Rails.application.routes.draw do
   get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
+
   get "users" => "users#index"
+  get "users/:id" => "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
