e16a5b59070927b5924b99b05050f5c0249508c0
mizuno <31834@toyota.kosen-ac.jp>
2022-02-23 00:54:08 +0900

    Add: ablity to edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,20 @@ class UsersController < ApplicationController
       render("signup")
     end
   end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice] = "Editing completed"
+      redirect_to("/users")
+    else
+      render("users/edit")
+    end
+  end
 end
