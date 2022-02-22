3b3a5a5c54fa1e575bf40ca68b07f2a96949ed63
mizuno <31834@toyota.kosen-ac.jp>
2022-02-23 00:06:03 +0900

    Add: signup finction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,14 @@ class UsersController < ApplicationController
 
   def signup
   end
+
+  def create
+    @post = User.new(name: params[:name], email: params[:email])
+    if @post.save
+      flash[:notice] = "Registration completed"
+      redirect_to("/users/#{@post.id}")
+    else
+      render("signup")
+    end
+  end
 end
