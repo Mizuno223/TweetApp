f87171eb6db91bec6d710eebbeb56de908ac9ee3
mizuno <31834@toyota.kosen-ac.jp>
2022-02-23 23:44:10 +0900

    Add: login_form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,16 +33,22 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
+
     if params[:image]
       @user.image_name = "#{@user.id}.jpg"
       image = params[:image]
       File.binwrite("public/user_images/#{@user.image_name}", image.read)
     end
+
     if @user.save
       flash[:notice] = "Editing completed"
       redirect_to("/users")
     else
       render("users/edit")
     end
+  end
+
+  def login_form
+    
   end
 end
