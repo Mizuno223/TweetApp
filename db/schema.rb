561432e5711ad954cc7ffde6a63cd6a77a71c744
mizuno <31834@toyota.kosen-ac.jp>
2022-02-23 17:19:47 +0900

    Fix: migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_20_122619) do
+ActiveRecord::Schema.define(version: 2022_02_23_063737) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_02_20_122619) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
