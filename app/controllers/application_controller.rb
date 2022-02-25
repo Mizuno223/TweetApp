849e4c6e7d8519aa2684d54075b2cfbcbf811219
mizuno <31834@toyota.kosen-ac.jp>
2022-02-25 16:38:38 +0900

    Fix: authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
     before_action :set_current_user
+
     def set_current_user
         @current_user = User.find_by(id: session[:user_id])
     end
 
-    before_action :authenticate_user
     def authenticate_user 
         if @current_user == nil
             flash[:notice] = "ログインが必要です"
@@ -16,6 +16,13 @@ class ApplicationController < ActionController::Base
         if @current_user
             flash[:notice] = "すでにログインしています"
             redirect_to("/posts/index")
+        end
+    end
+
+    def ensure_correct_user
+        if @current_user.id != params[:id].to_i
+          flash[:notice] = "権限がありません"
+          redirect_to("/posts/index")
         end
     end
 end
