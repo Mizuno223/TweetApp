28eb1c21327ba229c1e57dd40a32fecab9f3989b
mizuno <31834@toyota.kosen-ac.jp>
2022-02-25 18:15:40 +0900

    Add: like function

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,8 @@ Rails.application.routes.draw do
   get "login" => "users#login_form"
   post "login" => "users#login"
   post "logout" => "users#logout"
+
+  post "likes/:post_id/create" => "likes#create"
+  post "likes/:post_id/destroy" => "likes#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
